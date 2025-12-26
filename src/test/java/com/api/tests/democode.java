39ce3eb971881fa9741a8cde7c974dabd2e2b014
Ishaan <nashine.eshan1@gmail.com>
2025-12-26 16:56:53 +0530

    sample code

---
+++ src/test/java/com/api/tests/democode.java
@@ -0,0 +1,10 @@
+package com.api.tests;
+
+public class democode {
+
+	public static void main(String[] args) {
+
+		System.out.println("Your code has been successfully commited to git hub");
+	}
+
+}
