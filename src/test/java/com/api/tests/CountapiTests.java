91600027984328d54330a5738a391a0371497f01
Ishaan <nashine.eshan1@gmail.com>
2025-12-26 17:29:41 +0530

    feature1 modified

---
+++ src/test/java/com/api/tests/CountapiTests.java
@@ -0,0 +1,11 @@
+package com.api.tests;
+
+public class CountapiTests {
+
+	public static void main(String[] args) {
+		System.out.println("Count API Tests ");
+		System.out.println("spoken english");
+		System.out.println(" Capital markets");
+	}
+
+}
