05c1e48bab814788f4095487110ce1eda7d32fef
Ishaan <nashine.eshan1@gmail.com>
2025-12-29 15:39:33 +0530

    NEW CHANGES

---
+++ src/test/java/com/api/tests/Demoprgm.java
@@ -0,0 +1,12 @@
+package com.api.tests;
+
+public class Demoprgm {
+
+	public static void main(String[] args) {
+		int x = 5;
+		for (x = 0; x <= 5; x++) {
+			System.out.println("Hello This is  Java Coding");
+		}
+	}
+
+}
